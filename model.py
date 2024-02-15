5936a7b451ca09058a82d8750c45d8862175a051
MichaelChen0429 <69841155+MichaelChen0429@users.noreply.github.com>
2024-02-15 20:35:01 +0800

    feat__model

--- model.py
+++ model.py
@@ -1,33 +1,33 @@
 from openai import OpenAI
+from sk import OPENAI_API_KEY
 
-client = OpenAI()
+client = OpenAI(api_key=OPENAI_API_KEY)
+
+intstructions_string = "ShenNutritionGPT can provide dietary recommendations \
+based on your nutritional needs through your food preferences. \
+In addition to recommending basic ingredients, it can also suggest \
+suitable options for dining out and prepare recipes for you, \
+making it much easier for you to control your diet."
 
 assistant = client.beta.assistants.create(
-    name="Math Tutor",
-    instructions="You are a personal math tutor. Write and run code to answer math questions.",
-    tools=[{"type": "code_interpreter"}],
-    model="gpt-4-turbo-preview"
+    name="ShenNutritionGPT",
+    description="Food Nutrition GPT for Diet Recommendation",
+    instructions=intstructions_string,
+    model="gpt-3.5-turbo-0125"
 )
 
+# create thread (i.e. object that handles conversation between user and assistant)
 thread = client.beta.threads.create()
 
+# add a user message to the thread
 message = client.beta.threads.messages.create(
     thread_id=thread.id,
     role="user",
-    content="I need to solve the equation `3x + 11 = 14`. Can you help me?"
+    content="Great content, thank you!"
 )
 
+# send message to assistant to generate a response
 run = client.beta.threads.runs.create(
   thread_id=thread.id,
   assistant_id=assistant.id,
-  instructions="Please address the user as Jane Doe. The user has a premium account."
-)
-
-run = client.beta.threads.runs.retrieve(
-  thread_id=thread.id,
-  run_id=run.id
-)
-
-messages = client.beta.threads.messages.list(
-  thread_id=thread.id
 )

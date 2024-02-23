db755bf42da8d5d76d7b19abc4068c1ce613ba5d
MichaelChen0429 <69841155+MichaelChen0429@users.noreply.github.com>
2024-02-23 10:59:11 +0800

    update__api_key_method

--- src/POC_langchain_assistant_input_api_key.py
+++ src/POC_langchain_assistant_input_api_key.py
@@ -11,7 +11,7 @@ def main():
     st.set_page_config(page_title="Nourish X", page_icon="🥚")
     st.title("🥚 Nourish X")
     # OPENAI_APIKEY = os.environ.get("OPEN_AI_KEY")
-    openai_api_key = st.sidebar.text_input("OpenAI API Key", type="password")
+    openai_api_key_input = st.sidebar.text_input("OpenAI API Key", type="password")
 
     if 'messages' not in st.session_state:
         st.session_state["messages"] = [
@@ -25,23 +25,28 @@ def main():
         st.empty()
         st.chat_message("user").write(prompt)
         
-        
-        if not openai_api_key:
+        if not openai_api_key_input:
             st.info("Please add your OpenAI API key to continue.")
             st.stop()
         
-        assistant_id = "asst_kjXxune3YNpmyZ6dMbij9W1m"
-        agent = OpenAIAssistantRunnable(assistant_id=assistant_id, openai_api_key=openai_api_key, as_agent=True)
-        agent_executor = AgentExecutor(agent=agent, tools=get_tools())
-        with st.chat_message("assistant"):
-            st.write("🧠 thinking...")
-            if 'thread_id' not in st.session_state:
-                response = agent_executor.invoke({"content": prompt})
-                st.session_state.thread_id = response["thread_id"]
-            else:
-                response = agent_executor.invoke({"content": prompt, "thread_id": st.session_state.thread_id})
-            st.session_state.messages.append({"role": "assistant", "content": response["output"]})
-            st.write(response["output"])
+        openai_api_key = openai_api_key_input
+
+        try:
+            assistant_id = "asst_kjXxune3YNpmyZ6dMbij9W1m"
+            agent = OpenAIAssistantRunnable(assistant_id=assistant_id, openai_api_key=openai_api_key, as_agent=True)
+            agent_executor = AgentExecutor(agent=agent, tools=get_tools())
+            with st.chat_message("assistant"):
+                st.write("🧠 thinking...")
+                if 'thread_id' not in st.session_state:
+                    response = agent_executor.invoke({"content": prompt})
+                    st.session_state.thread_id = response["thread_id"]
+                else:
+                    response = agent_executor.invoke({"content": prompt, "thread_id": st.session_state.thread_id})
+                st.session_state.messages.append({"role": "assistant", "content": response["output"]})
+                st.write(response["output"])
+        except:
+            st.info("Your OpenAI API KEY is unavailable or not correct.")
+            st.stop()
 
 if __name__ == '__main__':
     main()
